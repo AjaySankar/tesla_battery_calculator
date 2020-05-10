8fab20644381ffcb34447050f0458680c3779772
Ajay Sankar <ajay0196@gmail.com>
2020-05-09 23:49:35 -0700

    Restructred..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import TeslaBattery from './components/TeslaBattery';
+import TeslaBattery from './containers/TeslaBattery';
 
 function App() {
   return (
